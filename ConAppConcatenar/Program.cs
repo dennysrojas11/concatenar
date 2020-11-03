65bdbd6fdbe17012a2d089af93d776a4809dfa27
Dennys Rojas <65422210+dennysrojas11@users.noreply.github.com>
2020-11-03 14:10:33 -0500

    Carga proyecto
    Carga proyecto en C# que concatena 3 cadenas de texto

---
+++ ConAppConcatenar/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ConAppConcatenar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String cadena, frase=null;
+
+            Console.WriteLine("Programa que concatena 3 cadenas de texto");
+            for (int i = 1; i <= 3; i++) {
+                Console.WriteLine("Ingrese la cadena: "+i);
+                cadena = Console.ReadLine();
+                if (validar(cadena) == true)
+                {
+                    frase += cadena;
+                }
+                else {
+                    Console.WriteLine("Solo se permite el ingreso de texto");
+                    i--;
+                }
+                
+            }
+            Console.WriteLine("Cadena concatenada: "+frase);
+            
+        }
+        
+        public static bool validar(string cadena) {
+            bool l = false;
+            for (int j = 0; j < cadena.Length; j++) {
+                if (char.IsLetter(cadena, j))
+                {
+                    l = true;
+                }
+                else {
+                    l = false;
+                    j = cadena.Length;
+                }
+            }
+            return (l);
+        }
+    }
+}
